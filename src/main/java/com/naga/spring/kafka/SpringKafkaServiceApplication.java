291d27e353ff788a929adb99a451e904cee93ef4
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-12-10 12:31:03 -0500

    Kafka Spring boot example

---
+++ src/main/java/com/naga/spring/kafka/SpringKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.naga.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naga/spring/kafka/SpringKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naga.spring.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
