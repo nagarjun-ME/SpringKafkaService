0adbeaf9e0bc16867d12600a4b9475f24153398f
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-12-10 13:28:27 -0500

    Updated with new controller

--- src/main/java/com/naga/spring/kafka/controller/KafkaController.java
+++ src/main/java/com/naga/spring/kafka/controller/KafkaController.java
@@ -1,6 +1,10 @@
 package com.naga.spring.kafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/naga/k")
 public class KafkaController {
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Value("${kafka.msg.topic}")
+    private String MSG_TOPIC ;
+
     @GetMapping("/")
     public String SayHello(){return  "Welcome to kafka example";}
+
+    @GetMapping("/{msg}")
+    public String publishMessage(@PathVariable ("msg") String msg){
+
+
+        kafkaTemplate.send(MSG_TOPIC,msg);
+        return  "Welcome to kafka example";
+    }
 }
